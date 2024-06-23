dce815aa1c0cd092a65e1d1a262c2d0bff0171e8
Scarletkibou <jr_pokemon@windowslive.com>
2024-06-24 01:25:23 +0700

    still learning

---
+++ lib/bloc/weather_bloc_bloc.dart
@@ -0,0 +1,25 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:learn_bloc/data/my_data.dart';
+import 'package:weather/weather.dart';
+
+part 'weather_bloc_event.dart';
+part 'weather_bloc_state.dart';
+
+class WeatherBlocBloc extends Bloc<WeatherBlocEvent, WeatherBlocState> {
+  WeatherBlocBloc() : super(WeatherBlocInitial()) {
+    on<FetchWeather>((event, emit) async {
+      emit(WeatherBlocLoading());
+      try {
+        WeatherFactory wf = WeatherFactory(API_KEY, language: Language.ENGLISH);
+        Position position = await Geolocator.getCurrentPosition();
+        Weather weather = await wf.currentWeatherByLocation(
+            position.latitude, position.longitude);
+        emit(WeatherBlocSuccess(weather));
+      } catch (e) {
+        emit(WeatherBlocFailure());
+      }
+    });
+  }
+}
---
+++ lib/data/my_data.dart
@@ -0,0 +1,1 @@
+String API_KEY = "2a958b06bdd08696174ac2f635adbb1f";
